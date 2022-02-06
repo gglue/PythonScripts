0d18c38a2ec334d80b067c88d1170b104573a30d
victor <victor@pop-os.localdomain>
2022-02-06 04:30:24 -0500

    Fixed formatting

--- crypto.py
+++ crypto.py
@@ -14,28 +14,35 @@ if (len(sys.argv) == 1):
     page = requests.get("https://coinmarketcap.com/")
     soup = BeautifulSoup(page.content, 'html.parser')
 
+    # Print table captions
+    print("%2s %-15s %-15s %-15s %-15s %-15s" % ("#", "Name", "Price", "Daily Change", "Weekly Change", "Market Cap"))
+
     # Extract the top 10 cryptocurrencies atm
-    rank = 1
-    ranking = []    
+    rank = 1  
     table = (soup.find("tbody"))
     for crypto in (table.find_all("tr")):
-        # Fetch rank, name and current price
-        print("%d. " % rank, end="")
-        print(crypto.find("p", class_="iworPT").text, end = "")
-        print(crypto.find("div", class_="cLgOOr").text, end = "")
+        # Fetch name and current price
+        name = crypto.find("p", class_="iworPT").text
+        price = crypto.find("div", class_="cLgOOr").text
 
         # Fetch change from past day and past week
         # The change daily and weekly have the same classname, so I used a for statement
+        changes = ""
         status = crypto.find_all("span", class_="sc-15yy2pl-0")
         for change in status:
             if(change.find("span", class_="icon-Caret-up")):
-                print("Up " + change.parent.text, end ="")
+                changes += ("Up " + change.parent.text + "!")
             else:
-                print("Down " + change.parent.text, end = "")
-
+                changes += ("Down " + change.parent.text + "!")
+        # Split the data up for better formatting later
+        changes = changes.split('!')
+        
         # Fetch Market Cap
-        print(crypto.find("span", class_="ieFnWP").text)
+        market = crypto.find("span", class_="iosgXe").text
 
+        # Print out the results
+        print("%2s %-15s %-15s %-15s %-15s %-15s" % (rank, name, price, changes[0], changes[1], market))
+        
         # Print up to the given rank value
         rank += 1
         if (rank > 10): break
